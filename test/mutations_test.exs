efc290e34e7f6918cda46e04e93d356d47482099
Jason Scott <dev.jason.scott@gmail.com>
2019-01-12 22:50:02 +0100

    finishes mutations tests

--- test/mutations_test.exs
+++ test/mutations_test.exs
@@ -5,28 +5,32 @@ defmodule MutationsTest do
 
   doctest Bigtable
 
-  @row_key "Test#123"
-
   setup do
-    [entry: Mutations.build(@row_key)]
+    [
+      entry: Mutations.build("Test#123"),
+      row_key: "Test#123",
+      family_name: "testFamily",
+      column_qualifier: "testColumn",
+      value: "test"
+    ]
   end
 
   describe "Mutations.build " do
     test "should build a MutateRowsRequest Entry struct", context do
       expected = %Entry{
         mutations: [],
-        row_key: @row_key
+        row_key: context.row_key
       }
 
       assert context.entry == expected
     end
   end
 
-  describe "Mutations.SetCell" do
+  describe "Mutations.set_cell" do
     test "should return a SetCell struct", context do
-      family_name = "testFamily"
-      column_qualifier = "testColumn"
-      value = "test"
+      family_name = context.family_name
+      column_qualifier = context.column_qualifier
+      value = context.value
 
       expected = %Entry{
         mutations: [
@@ -41,10 +45,84 @@ defmodule MutationsTest do
                }}
           }
         ],
-        row_key: @row_key
+        row_key: context.row_key
       }
 
       result = context.entry |> Mutations.set_cell(family_name, column_qualifier, value)
+
+      assert result == expected
+    end
+  end
+
+  describe "Mutations.delete_from_column" do
+    test "should return a DeleteFromColumn struct", context do
+      entry = context.entry
+      row_key = context.row_key
+      family_name = context.family_name
+      column_qualifier = context.column_qualifier
+
+      expected = %Google.Bigtable.V2.MutateRowsRequest.Entry{
+        mutations: [
+          %Google.Bigtable.V2.Mutation{
+            mutation:
+              {:delete_from_column,
+               %Google.Bigtable.V2.Mutation.DeleteFromColumn{
+                 family_name: family_name,
+                 column_qualifier: column_qualifier,
+                 time_range: %Google.Bigtable.V2.TimestampRange{
+                   end_timestamp_micros: 0,
+                   start_timestamp_micros: 0
+                 }
+               }}
+          }
+        ],
+        row_key: context.row_key
+      }
+
+      result = context.entry |> Mutations.delete_from_column(family_name, column_qualifier)
+
+      assert result == expected
+    end
+  end
+
+  describe "Mutations.delete_from_family" do
+    test "should return a DeleteFromFamily struct", context do
+      entry = context.entry
+      row_key = context.row_key
+      family_name = context.family_name
+
+      expected = %Google.Bigtable.V2.MutateRowsRequest.Entry{
+        mutations: [
+          %Google.Bigtable.V2.Mutation{
+            mutation:
+              {:delete_from_family,
+               %Google.Bigtable.V2.Mutation.DeleteFromFamily{family_name: "testFamily"}}
+          }
+        ],
+        row_key: "Test#123"
+      }
+
+      result = context.entry |> Mutations.delete_from_family(family_name)
+
+      assert result == expected
+    end
+  end
+
+  describe "Mutations.delete_from_row" do
+    test "should return a DeleteFromRow struct", context do
+      entry = context.entry
+      row_key = context.row_key
+
+      expected = %Google.Bigtable.V2.MutateRowsRequest.Entry{
+        mutations: [
+          %Google.Bigtable.V2.Mutation{
+            mutation: {:delete_from_row, %Google.Bigtable.V2.Mutation.DeleteFromRow{}}
+          }
+        ],
+        row_key: "Test#123"
+      }
+
+      result = context.entry |> Mutations.delete_from_row()
 
       assert result == expected
     end
