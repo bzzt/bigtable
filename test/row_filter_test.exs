d5fc00492c72cef73cd296755e22e02ed2a97abb
Jason Scott <dev.jason.scott@gmail.com>
2019-01-13 12:32:07 +0100

    adds row filter tests

--- test/row_filter_test.exs
+++ test/row_filter_test.exs
@@ -7,7 +7,13 @@ defmodule RowFilterTest do
     [
       request: %Google.Bigtable.V2.ReadRowsRequest{
         app_profile_id: "",
-        filter: nil,
+        filter: %Google.Bigtable.V2.RowFilter{
+          filter:
+            {:chain,
+             %Google.Bigtable.V2.RowFilter.Chain{
+               filters: []
+             }}
+        },
         rows: nil,
         rows_limit: 0,
         table_name: Bigtable.Utils.configured_table_name()
@@ -55,6 +61,21 @@ defmodule RowFilterTest do
   end
 
   describe "RowFilter.cells_per_column()" do
+    test "should apply a cells per column V2.RowFilter to a V2.ReadRowsRequest", context do
+      expected = expected_request(context.filter)
+
+      assert RowFilter.cells_per_column(context.request, 1) == expected
+    end
+
+    test "should return a V2.RowFilter given a column limit" do
+      column_limit = 5
+
+      expected = %Google.Bigtable.V2.RowFilter{
+        filter: {:cells_per_column_limit_filter, column_limit}
+      }
+
+      assert RowFilter.cells_per_column(column_limit) == expected
+    end
   end
 
   defp expected_chain(filters) when is_list(filters) do
