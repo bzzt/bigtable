56db4ba2a22210f6d142ee7adfb9a89d1dc1a482
Jason Scott <dev.jason.scott@gmail.com>
2019-01-15 07:50:51 +0100

    foo

--- config/dev.exs
+++ config/dev.exs
@@ -1,5 +1,5 @@
 # This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
+# and its dependencies with the aid of the Mix.Config module.s
 use Mix.Config
 
 config :pre_commit,
