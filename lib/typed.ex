83bba847b8f70694050096bdf39dc115863962d3
Jason Scott <dev.jason.scott@gmail.com>
2019-01-18 10:15:10 +0100

    child type working

--- lib/typed.ex
+++ lib/typed.ex
@@ -1,124 +1,6 @@
 defmodule Bigtable.Typed do
   alias Bigtable.ByteString
 
-  def test_parse() do
-    type_spec = %{
-      family_one: %{
-        one_a: :integer,
-        one_b: :boolean,
-        one_nested: %{
-          one_nested_a: :integer,
-          one_nested_b: :boolean
-        },
-        one_double_nested: %{
-          one_double_nested_a: %{
-            a: :integer
-          }
-        }
-      },
-      family_two: %{
-        two_a: :integer,
-        two_b: :boolean
-      }
-    }
-
-    chunks = [
-      %Google.Bigtable.V2.ReadRowsResponse.CellChunk{
-        family_name: %Google.Protobuf.StringValue{value: "family_one"},
-        labels: [],
-        qualifier: %Google.Protobuf.BytesValue{value: "one_a"},
-        row_key: "Row#123",
-        row_status: {:commit_row, true},
-        timestamp_micros: 1_547_637_474_930_000,
-        value: ByteString.to_byte_string(1),
-        value_size: 0
-      },
-      %Google.Bigtable.V2.ReadRowsResponse.CellChunk{
-        family_name: %Google.Protobuf.StringValue{value: "family_one"},
-        labels: [],
-        qualifier: %Google.Protobuf.BytesValue{value: "one_b"},
-        row_key: "Row#123",
-        row_status: {:commit_row, true},
-        timestamp_micros: 1_547_637_474_930_000,
-        value: ByteString.to_byte_string(true),
-        value_size: 0
-      },
-      %Google.Bigtable.V2.ReadRowsResponse.CellChunk{
-        family_name: %Google.Protobuf.StringValue{value: "family_one"},
-        labels: [],
-        qualifier: %Google.Protobuf.BytesValue{value: "one_nested.one_nested_a"},
-        row_key: "Row#123",
-        row_status: {:commit_row, true},
-        timestamp_micros: 1_547_637_474_930_000,
-        value: ByteString.to_byte_string(1),
-        value_size: 0
-      },
-      %Google.Bigtable.V2.ReadRowsResponse.CellChunk{
-        family_name: %Google.Protobuf.StringValue{value: "family_one"},
-        labels: [],
-        qualifier: %Google.Protobuf.BytesValue{value: "one_nested.one_nested_b"},
-        row_key: "Row#123",
-        row_status: {:commit_row, true},
-        timestamp_micros: 1_547_637_474_930_000,
-        value: ByteString.to_byte_string(true),
-        value_size: 0
-      },
-      %Google.Bigtable.V2.ReadRowsResponse.CellChunk{
-        family_name: %Google.Protobuf.StringValue{value: "family_one"},
-        labels: [],
-        qualifier: %Google.Protobuf.BytesValue{value: "one_double_nested.one_double_nested_a.a"},
-        row_key: "Row#123",
-        row_status: {:commit_row, true},
-        timestamp_micros: 1_547_637_474_930_000,
-        value: ByteString.to_byte_string(1),
-        value_size: 0
-      },
-      %Google.Bigtable.V2.ReadRowsResponse.CellChunk{
-        family_name: %Google.Protobuf.StringValue{value: "family_two"},
-        labels: [],
-        qualifier: %Google.Protobuf.BytesValue{value: "two_a"},
-        row_key: "Row#123",
-        row_status: {:commit_row, true},
-        timestamp_micros: 1_547_637_474_930_000,
-        value: ByteString.to_byte_string(1),
-        value_size: 0
-      },
-      %Google.Bigtable.V2.ReadRowsResponse.CellChunk{
-        family_name: %Google.Protobuf.StringValue{value: "family_two"},
-        labels: [],
-        qualifier: %Google.Protobuf.BytesValue{value: "two_b"},
-        row_key: "Row#123",
-        row_status: {:commit_row, true},
-        timestamp_micros: 1_547_637_474_930_000,
-        value: ByteString.to_byte_string(true),
-        value_size: 0
-      }
-    ]
-
-    parse_typed(type_spec, chunks)
-  end
-
-  def test_mut do
-    map = %{
-      family_a: %{
-        id: "id#123",
-        age: 32,
-        nested: %{
-          first: "first",
-          second: "second"
-        },
-        double: %{
-          nested: %{
-            first: true,
-            second: 2
-          }
-        }
-      }
-    }
-
-    create_mutations("id#123", map)
-  end
-
   def create_mutations(row_key, map) do
     entry = Bigtable.Mutations.build(row_key)
 
@@ -152,8 +34,8 @@ defmodule Bigtable.Typed do
     end)
   end
 
-  def parse_typed(type_spec, chunks) do
-    Enum.reduce(chunks, %{}, fn chunk, accum ->
+  def parse_typed(type_spec, row) do
+    Enum.reduce(row.chunks, %{}, fn chunk, accum ->
       family_key = String.to_atom(chunk.family_name.value)
       family_spec = Map.fetch!(type_spec, family_key)
 
