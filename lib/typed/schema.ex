9c6d478d1b4122f41ed967e4201afa0df2490c88
Jason Scott <dev.jason.scott@gmail.com>
2019-01-20 14:49:20 +0100

    refactors schema macro

--- lib/typed/schema.ex
+++ lib/typed/schema.ex
@@ -2,20 +2,17 @@ defmodule Bigtable.Schema do
   defmacro __using__(_opt) do
     quote do
       import unquote(__MODULE__)
+      Module.register_attribute(__MODULE__, :rows, accumulate: true)
+      Module.register_attribute(__MODULE__, :families, accumulate: true)
     end
   end
 
   defmacro type(do: block) do
-    columns =
-      case block do
-        {:__block__, [], multiple} -> multiple
-        {:column, _, [name, block]} -> [{:column, [], [name, block]}]
-      end
-
-    column_list = list_from_block(columns, :column)
-
     quote do
-      defstruct unquote(column_list)
+      var!(columns) = []
+      unquote(block)
+
+      defstruct var!(columns)
 
       def type() do
         %__MODULE__{}
@@ -24,24 +21,9 @@ defmodule Bigtable.Schema do
   end
 
   defmacro row(do: block) do
-    families =
-      case block do
-        {:__block__, [], multiple} -> multiple
-        {:family, _, [name, block]} -> [{:family, [], [name, block]}]
-      end
-
-    family_list = list_from_block(families, :family)
-
-    families_with_columns =
-      Enum.map(family_list, fn family ->
-        {family_name, [do: {:__block__, [], columns}]} = family
-        column_list = list_from_block(columns, :column)
-
-        {family_name, Map.new(column_list)}
-      end)
-
     quote do
-      defstruct unquote(Macro.escape(families_with_columns))
+      unquote(block)
+      defstruct @families
 
       def parse(row) do
         Bigtable.Typed.parse_typed(__MODULE__.type(), row)
@@ -53,19 +35,20 @@ defmodule Bigtable.Schema do
     end
   end
 
-  defmacro family(name, do: block), do: {name, block}
+  defmacro family(name, do: block) do
+    quote do
+      var!(columns) = []
+      unquote(block)
+      @families {unquote(name), Map.new(var!(columns))}
+    end
+  end
 
-  defmacro column(key, value), do: {key, value}
+  defmacro column(key, value) do
+    c = {key, get_value_type(value)} |> Macro.escape()
 
-  defp list_from_block(block, to_match) do
-    Enum.reduce(block, [], fn value, accum ->
-      with {block_type, _, [k, v]} <- value do
-        case block_type == to_match do
-          true -> [{k, get_value_type(v)} | accum]
-          false -> accum
-        end
-      end
-    end)
+    quote do
+      var!(columns) = [unquote(c) | var!(columns)]
+    end
   end
 
   defp get_value_type(value) when is_atom(value), do: value
@@ -73,8 +56,24 @@ defmodule Bigtable.Schema do
   defp get_value_type({:__aliases__, _, modules}) do
     Module.concat([Elixir | modules]).type()
   end
+end
 
-  defp get_value_type(value) do
-    value
+defmodule TestType do
+  use Bigtable.Schema
+
+  type do
+    column(:a, :integer)
+  end
+end
+
+defmodule TestSchema do
+  use Bigtable.Schema
+
+  row do
+    family :my_family do
+      column(:foo, :integer)
+      column(:bar, :float)
+      column(:type, TestType)
+    end
   end
 end
