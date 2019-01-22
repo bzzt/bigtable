f1b8134a030bfd9ac5d85e378fdf05b17c724b15
Jason Scott <dev.jason.scott@gmail.com>
2019-01-22 09:06:58 +0100

    refactors operations and adds delete

---
+++ lib/typed/operations/delete.ex
@@ -0,0 +1,14 @@
+defmodule Bigtable.Typed.Delete do
+  # TODO: Delete extra rowkey patterns
+  alias Bigtable.{MutateRows, Mutations}
+
+  def delete_rows(ids, row_prefix) do
+    Enum.map(ids, &delete_row(&1, row_prefix))
+    |> MutateRows.mutate()
+  end
+
+  defp delete_row(id, row_prefix) do
+    Mutations.build("#{row_prefix}##{id}")
+    |> Mutations.delete_from_row()
+  end
+end
