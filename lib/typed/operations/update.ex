544ba2ef09f7d42582cf5ede08bbe9baa6109249
Jason Scott <dev.jason.scott@gmail.com>
2019-01-22 11:26:07 +0100

    starts working on delete all

--- lib/typed/operations/update.ex
+++ lib/typed/operations/update.ex
@@ -1,18 +1,19 @@
 defmodule Bigtable.Typed.Update do
-  alias Bigtable.Typed.Utils
+  alias Bigtable.{MutateRows, Typed}
+  alias Typed.Utils
 
   def mutations_from_maps(maps, row_prefix, update_patterns) do
     Enum.map(maps, &mutations_from_map(&1, row_prefix, update_patterns))
     |> List.flatten()
-    |> Bigtable.MutateRows.build()
-    |> Bigtable.MutateRows.mutate()
+    |> MutateRows.build()
+    |> MutateRows.mutate()
   end
 
   defp mutations_from_map(map, row_prefix, update_patterns) do
     Enum.map(update_patterns, fn pattern ->
       Utils.row_key_properties(pattern)
       |> Utils.build_update_key(row_prefix, map)
-      |> Bigtable.Typed.create_mutations(map)
+      |> Typed.create_mutations(map)
     end)
   end
 end
