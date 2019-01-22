4f674d44922756944be3ba3630e30bafff351d61
Jason Scott <dev.jason.scott@gmail.com>
2019-01-22 22:19:58 +0100

    refactors bytestring module to use multi headed functions vs single functions with case

--- lib/byte_string.ex
+++ lib/byte_string.ex
@@ -1,61 +1,53 @@
 defmodule Bigtable.ByteString do
   @moduledoc false
-  @spec parse_value(:boolean | :float | :integer | :list | :map | :string, binary()) ::
-          integer() | float() | binary() | boolean() | map() | list()
-  def parse_value(type, byte_string) do
-    case type do
-      :integer ->
-        <<v::integer-signed-32>> = byte_string
-        v
 
-      :float ->
-        <<v::signed-little-float-64>> = byte_string
-        v
+  @spec parse_value(:integer, binary()) :: integer()
+  def parse_value(:integer, byte_string) do
+    <<v::integer-signed-32>> = byte_string
+    v
+  end
 
-      :string ->
-        to_string(byte_string)
+  @spec parse_value(:float, binary()) :: float()
+  def parse_value(:float, byte_string) do
+    <<v::signed-little-float-64>> = byte_string
+    v
+  end
 
-      :boolean ->
-        case parse_value(:integer, byte_string) do
-          0 -> false
-          1 -> true
-        end
+  @spec parse_value(:string, binary()) :: binary()
+  def parse_value(:string, byte_string) do
+    to_string(byte_string)
+  end
 
-      :map ->
-        Poison.decode!(byte_string)
+  @spec parse_value(:map, binary()) :: map()
+  def parse_value(:map, byte_string) do
+    Poison.decode!(byte_string)
+  end
 
-      :list ->
-        Poison.decode!(byte_string)
+  @spec parse_value(:list, binary()) :: list()
+  def parse_value(:list, byte_string) do
+    Poison.decode!(byte_string)
+  end
+
+  @spec to_byte_string(nil) :: binary()
+  def to_byte_string(v) when is_nil(v), do: ""
+
+  @spec to_byte_string(binary()) :: binary()
+  def to_byte_string(v) when is_binary(v), do: v
+
+  @spec to_byte_string(boolean()) :: binary()
+  def to_byte_string(v) when is_boolean(v) do
+    case v do
+      true -> to_byte_string(1)
+      false -> to_byte_string(0)
     end
   end
 
-  @spec to_byte_string(false | nil | true | binary() | list() | map() | integer() | float()) ::
-          binary()
-  def to_byte_string(value) do
-    case value do
-      v when is_nil(v) ->
-        ""
+  @spec to_byte_string(integer()) :: binary()
+  def to_byte_string(v) when is_integer(v), do: <<v::integer-signed-32>>
 
-      v when is_binary(v) ->
-        v
+  @spec to_byte_string(float()) :: binary()
+  def to_byte_string(v) when is_float(v), do: <<v::signed-little-float-64>>
 
-      v when is_boolean(v) ->
-        case v do
-          true -> to_byte_string(1)
-          false -> to_byte_string(0)
-        end
-
-      v when is_integer(v) ->
-        <<v::integer-signed-32>>
-
-      v when is_float(v) ->
-        <<v::signed-little-float-64>>
-
-      v when is_list(v) ->
-        Poison.encode!(v)
-
-      v when is_map(v) ->
-        Poison.encode!(v)
-    end
-  end
+  @spec to_byte_string(list() | map()) :: binary()
+  def to_byte_string(v) when is_list(v) or is_map(v), do: Poison.encode!(v)
 end
