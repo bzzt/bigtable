f649bdba1bbbb7814d81de74fdd1f5398da7fe46
Jason Scott <dev.jason.scott@gmail.com>
2019-04-24 09:11:08 +0200

    refactors utils

--- lib/utils.ex
+++ lib/utils.ex
@@ -1,6 +1,7 @@
 defmodule Bigtable.Utils do
   @moduledoc false
 
+  @spec configured_table_name() :: binary()
   def configured_table_name do
     instance = configured_instance_name()
     table = Application.get_env(:bigtable, :table)
@@ -8,16 +9,19 @@ defmodule Bigtable.Utils do
     "#{instance}/tables/#{table}"
   end
 
+  @spec configured_instance_name() :: binary()
   def configured_instance_name do
     project = get_project()
     instance = get_instance()
     "projects/#{project}/instances/#{instance}"
   end
 
+  @spec get_project() :: binary()
   defp get_project do
     Application.get_env(:bigtable, :project)
   end
 
+  @spec get_instance() :: binary()
   defp get_instance do
     Application.get_env(:bigtable, :instance)
   end
